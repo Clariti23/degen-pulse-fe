afbddc0aa069519a3f031ce37c5ecb1f9fe4c19d
clariti23 <g.ferowich@gmail.com>
2020-10-17 10:52:48 -0500

    successfully pull projects from server api

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import './App.css';
 import ProjectTable from './Components/ProjectTable';
 
-function App() {
+
+
+
+const App = () => {
+
+  
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
